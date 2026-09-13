aebdd1690e608db5f792aa282c3d5f748e005b07
srikanth-bitdynamics <259878899+srikanth-bitdynamics@users.noreply.github.com>
2026-09-13 12:35:02 +0400

    Hoist waitPoint and IdPlaceholder into postgres.backend

--- modules/postgres/backend/src/com/digitalasset/pqs/postgres/backend/package.scala
+++ modules/postgres/backend/src/com/digitalasset/pqs/postgres/backend/package.scala
@@ -7,10 +7,13 @@ import com.digitalasset.pqs.o11y.traces
 import com.digitalasset.pqs.postgres.backend.TlsConfig.SslMode
 import org.postgresql.PGProperty
 import zio.jdbc.*
-import zio.stream.{ZPipeline, ZSink}
-import zio.{ZIO, ZLayer}
+import zio.metrics.Metric
+import zio.metrics.MetricKeyType.Histogram.Boundaries
+import zio.stream.{ZChannel, ZPipeline, ZSink, ZStream}
+import zio.{Chunk, Exit, Queue, ZIO, ZLayer}
 
 import java.io.File
+import java.util.concurrent.atomic.AtomicLong
 
 package object backend:
   /** Execute each transaction in parallel */
@@ -84,5 +87,45 @@ package object backend:
   private def sslkey(conf: TlsConfig)      = fromFile(conf.privateKey, PGProperty.SSL_KEY)
 
   private def fromFile(file: Option[File], key: PGProperty) = file.map(f => key.getName -> f.getCanonicalPath)
+
+  /** Bounded async buffer between upstream and downstream with back-pressure; depth is tracked as `<name>_size`. */
+  def waitPoint[A](name: String, capacity: Int = 16, chunkSize: Int = 16): ZPipeline[Any, Nothing, A, A] =
+    ZPipeline.fromFunction[Any, Nothing, A, A](inStream =>
+      ZStream.unwrapScoped(
+        ZIO
+          .acquireRelease(
+            Queue.bounded[Exit[Option[Nothing], A]](capacity) <&> zio.Ref.make(0)
+          )(
+            _._1.shutdown
+          )
+          .flatMap { (queue, qSize) =>
+            val size = Metric.histogram(
+              s"${name}_size",
+              s"Number of in-flight units of work in $name wait point",
+              Boundaries.linear(0, capacity.toDouble / 16, 16)
+            )
+            val counter           = Metric.counter(name, s"Number of units of work processed in $name wait point")
+            def inc(as: Chunk[?]) = qSize.updateAndGet(_ + as.size).flatMap(size.update(_)) *> counter.modify(as.size)
+            def dec(as: Chunk[?]) = qSize.updateAndGet(_ - as.size).flatMap(size.update(_))
+
+            val outStream = ZStream.fromQueue(queue, chunkSize).flattenExitOption.mapChunksZIO(ch => dec(ch).as(ch))
+
+            lazy val enqueue: ZChannel[Any, Nothing, Chunk[A], Any, Nothing, Nothing, Any] =
+              ZChannel.readWithCause[Any, Nothing, Chunk[A], Any, Nothing, Nothing, Any](
+                in => ZChannel.fromZIO(inc(in).as(in.map(Exit.succeed)).flatMap(queue.offerAll)) *> enqueue,
+                err => ZChannel.fromZIO(queue.offer(Exit.failCause(err))) *> ZChannel.refailCause(err),
+                done => ZChannel.fromZIO(queue.offer(Exit.fail(None))) *> ZChannel.succeedNow(done)
+              )
+            (inStream.channel >>> enqueue).runScoped.forkScoped.as(outStream)
+          }
+      )
+    )
+
+  final case class IdPlaceholder private (id: Long)
+  object IdPlaceholder:
+    trait Factory { def mk: IdPlaceholder }
+    def factory(start: Long): Factory = new Factory:
+      private val cnt       = AtomicLong(start)
+      def mk: IdPlaceholder = IdPlaceholder(cnt.incrementAndGet())
 
 end backend
